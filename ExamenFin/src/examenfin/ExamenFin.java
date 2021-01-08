edb70d23d702eb2fce15af14df8ec8a6e9191089
Yael <coronadosegundojuanyael@gmail.com>
2021-01-08 00:14:11 -0600

    Examen

--- ExamenFin/src/examenfin/ExamenFin.java
+++ ExamenFin/src/examenfin/ExamenFin.java
@@ -5,5 +5,6 @@ public class ExamenFin {
    public static void main(String[] args) {
         VentanaInicio ventana = new VentanaInicio();
         ventana.setVisible(true);
+        //ENLACE DE GIT https://github.com/JuanYaelCS/ExamenFinal.git
     }
 }
